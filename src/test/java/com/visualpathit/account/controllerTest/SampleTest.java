6ffb6a1b944c41b1e6cb047c39e05f3af9d314f9
esref45 <esrefcimen@gmail.com>
2024-11-13 13:07:08 +0300

    Update SampleTest.java

--- src/test/java/com/visualpathit/account/controllerTest/SampleTest.java
+++ src/test/java/com/visualpathit/account/controllerTest/SampleTest.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SampleTest {
-	@Test
-	public void SampleTestHappyFlow(){
-		assertEquals("Hello".length(), 5);
-	}
 
+    @Test
+    public void shouldReturnCorrectStringLength() {
+        String sampleText = "Hello";
+        int expectedLength = 5;
+
+        assertEquals("String length should be 5", expectedLength, sampleText.length());
+    }
 }
